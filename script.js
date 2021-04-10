685e4db6a5504779f56b7955af48eeda1a45bdac
anurag <anurag@DESKTOP-JMF21M6>
2021-04-11 00:28:43 +0530

    page and navigation script is ready

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+	console.log("Hello world"); 
+	data = '<a href="index.html">0</a> <a href="page-1.html">1</a> <a href="page-2.html">2</a> XX Last Updated: 04/2021';
+	document.getElementById("pages").innerHTML = data;
+};
