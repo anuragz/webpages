be991b9cda607f6d6d2ea5b24edaa62fc582b47b
anurag <anurag@DESKTOP-JMF21M6>
2021-04-11 16:11:07 +0530

    some content and related files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+	console.log("Hello world"); 
+	data = '<a href="index.html">0</a> <a href="page-1.html">1</a> <a href="page-2.html">2</a>  <a href="page-3.html">3</a>  <a href="page-4.html">4</a>  <a href="page-5.html">5</a> <a href="page-6.html">6</a> <a href="page-7.html">7</a> <a href="page-8.html">8</a> <a href="page-9.html">9</a> <a href="page-10.html">10</a>';
+	document.getElementById("pages").innerHTML = data;
+};
